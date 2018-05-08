8178018e6d24fdce57263f86aedcd8fdd4b6faf9
breadone <bbangwon.chung@gamil.com>
2018-05-08 20:45:32 +0900

    유니티로 만드는 네트워크 게임 개발 - Chapter 2까지

---
+++ Assets/Scripts/DataFetch.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NCMB;
+
+public class DataFetch : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        FetchScoreList(10);
+
+    }
+
+    private void FetchScoreList(int higherThan)
+    {
+        NCMBQuery<NCMBObject> query = new NCMBQuery<NCMBObject>("PlayerData");
+        query.WhereGreaterThan("Score", higherThan);
+        query.FindAsync((List<NCMBObject> objList, NCMBException e) =>
+        {
+            if (e != null)
+            {
+                //오류시 처리
+            }
+            else
+            {
+                foreach (NCMBObject obj in objList)
+                {
+                    Debug.Log("playerName:" + obj["PlayerName"] +
+                              ", Score: " + obj["Score"] +
+                              ", Coin: " + obj["Coin"]);
+                }
+            }
+        });
+    }
+	
+
+}
---
+++ Assets/Scripts/DataSave.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NCMB;
+
+public class DataSave : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        SavePlayerData("Mina", 3, 60);
+
+    }
+	
+    public void SavePlayerData(string playerName, int score, int coin)
+    {
+        NCMBObject obj = new NCMBObject("PlayerData");
+
+        obj.Add("PlayerName", playerName);
+        obj.Add("Score", score);
+        obj.Add("Coin", coin);
+
+        obj.SaveAsync((NCMBException e) =>
+        {
+            if( e != null)
+            {
+                //오류처리
+                Debug.Log("저장 실패. 통신 환경을 확인하십시오.");
+            }
+            else
+            {
+                //성공시 처리
+                Debug.Log("저장 성공");
+            }
+
+        });
+    }
+}
